87d1aa9ee3b1d10ab518494d366eb8e735218eb1
mobile_app_bmi <gerhardkotze0071@gmail.com>
2021-06-23 21:41:58 +0200

    People Counter - or count something else

---
+++ app/src/main/java/com/gerhardkotze0071/helloworld00/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.gerhardkotze0071.helloworld00
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btnClickMe = findViewById(R.id.button) as Button
+        val myTextView = findViewById(R.id.textView) as TextView
+        var timesClicked = 0
+
+        // set on-click listener
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            myTextView.text = timesClicked.toString()
+            Toast.makeText(this@MainActivity, "Count People!.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
